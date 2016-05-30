142f70a0907fd556bda546bf07289921e3f1b499
Michael Kreimer <michaelkreimer@gmail.com>
2016-05-30 20:22:37 +0300

    make the code more readable

--- B16 Ex05 MichaelKreimer 305597478 IdoPerry 036928646/Ex05_WinformUi/Settings.cs
+++ B16 Ex05 MichaelKreimer 305597478 IdoPerry 036928646/Ex05_WinformUi/Settings.cs
@@ -17,9 +17,9 @@ namespace Ex05_WinformUi
         Label m_LabelBoardSize = new Label();
         Label m_LabelCols = new Label();
         Label m_LabelRows = new Label();
-        TextBox m_TextBoxPlayer1 = new TextBox();
-        TextBox m_TextBoxPlayer2 = new TextBox();
-        CheckBox m_CheckBoxPlayerHuman = new CheckBox();
+        TextBox m_TBPlayer1 = new TextBox();
+        TextBox m_TBPlayer2 = new TextBox();
+        CheckBox m_CBPlayerHuman = new CheckBox();
         NumericUpDown m_NUDRows = new NumericUpDown();
         NumericUpDown m_NUDCols = new NumericUpDown();
         public Settings()
@@ -33,57 +33,37 @@ namespace Ex05_WinformUi
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
-
+            this.Text = "Game Settings";
+            this.Size = new Size(300, 300);
             InitControls();
         }
 
         private void InitControls()
         {
-            this.Text = "Game Settings";
-            this.Size = new Size(300, 300);
-            m_ButtonStart.Text = "Start!";
-            m_ButtonStart.Location = new Point(this.Size.Width - 100, this.Size.Height - 70);
-            m_LabelPlayers.Text = "Players:";
+            initControl(m_ButtonStart, "Start!", Size.Width - 100, Size.Height - 70);
+            initControl(m_LabelPlayers, "Players:", 20, 10);
             m_LabelPlayers.Font = new Font("Arial", 10, FontStyle.Bold);
-            m_LabelPlayers.Location = new Point(20, 10);
-            m_LabelPlayer1.Text = "Player 1:";
-            m_LabelPlayer1.Location = new Point(m_LabelPlayers.Location.X + 30, m_LabelPlayers.Location.Y + 30);
-            m_TextBoxPlayer1.Location = new Point(m_LabelPlayer1.Location.X + 100, m_LabelPlayer1.Location.Y);
-            m_LabelPlayer2.Text = "Player 2:";
-            m_LabelPlayer2.Location = new Point(m_LabelPlayer1.Location.X, m_LabelPlayer1.Location.Y + 30);
-            m_TextBoxPlayer2.Location = new Point(m_TextBoxPlayer1.Location.X, m_LabelPlayer2.Location.Y);
-            m_TextBoxPlayer2.Text = "[Computer]";
-            m_TextBoxPlayer2.Enabled = false;
-            m_CheckBoxPlayerHuman.Location = new Point(m_LabelPlayer2.Location.X - 20, m_LabelPlayer2.Location.Y - 5);
+            initControl(m_LabelPlayer1, "Player 1:", m_LabelPlayers.Location.X + 30, m_LabelPlayers.Location.Y + 30);
+            initControl(m_TBPlayer1, String.Empty, m_LabelPlayer1.Location.X + 100, m_LabelPlayer1.Location.Y);
+            initControl(m_LabelPlayer2, "Player 2:", m_LabelPlayer1.Location.X, m_LabelPlayer1.Location.Y + 30);
+            initControl(m_TBPlayer2, "[Computer]", m_TBPlayer1.Location.X, m_LabelPlayer2.Location.Y);
+            m_TBPlayer2.Enabled = false;
+            initControl(m_CBPlayerHuman, String.Empty, m_LabelPlayer2.Location.X - 20, m_LabelPlayer2.Location.Y - 5);
+            initControl(m_LabelBoardSize, "Board Size:", m_LabelPlayers.Location.X, Size.Height / 3);
             m_LabelBoardSize.Font = m_LabelPlayers.Font;
-            m_LabelBoardSize.Text = "Board Size:";
-            m_LabelBoardSize.Location = new Point(m_LabelPlayers.Location.X , this.Size.Height / 3);
-            m_LabelRows.Text = "Rows:";
-            m_LabelRows.Location = new Point(m_LabelBoardSize.Location.X + 25, m_LabelBoardSize.Location.Y + 30);
-            m_LabelCols.Text = "Cols:";
-            m_LabelCols.Location = new Point(m_LabelRows.Location.X + 150, m_LabelRows.Location.Y);
+            initControl(m_LabelRows, "Rows:", m_LabelBoardSize.Location.X + 25, m_LabelBoardSize.Location.Y + 30);
+            initControl(m_LabelCols, "Cols:", m_LabelRows.Location.X + 150, m_LabelRows.Location.Y);
+            initControl(m_NUDCols, String.Empty, m_LabelCols.Location.X, m_LabelCols.Location.Y + 25);
+            initControl(m_NUDRows, String.Empty, m_LabelRows.Location.X, m_LabelRows.Location.Y + 25);
             m_NUDCols.Minimum = m_NUDRows.Minimum = 4;
             m_NUDCols.Size = m_NUDRows.Size = new Size(40, 20);
             m_NUDCols.Maximum = m_NUDRows.Maximum = 8;
-            m_NUDCols.Location = new Point(m_LabelCols.Location.X, m_LabelCols.Location.Y + 25);
-            m_NUDRows.Location = new Point(m_LabelRows.Location.X, m_LabelRows.Location.Y + 25);
-            addControls();
         }
-
-        private void addControls()
+        private void initControl(Control i_Label, string i_Text,int x,int y)
         {
-            this.Controls.Add(m_LabelPlayer1);
-            this.Controls.Add(m_LabelPlayer2);
-            this.Controls.Add(m_CheckBoxPlayerHuman);
-            this.Controls.Add(m_ButtonStart);
-            this.Controls.Add(m_LabelPlayers);
-            this.Controls.Add(m_TextBoxPlayer1);
-            this.Controls.Add(m_TextBoxPlayer2);
-            this.Controls.Add(m_LabelBoardSize);
-            this.Controls.Add(m_LabelRows);
-            this.Controls.Add(m_LabelCols);
-            this.Controls.Add(m_NUDRows);
-            this.Controls.Add(m_NUDCols);
+            i_Label.Text = i_Text;
+            i_Label.Location = new Point(x, y);
+            Controls.Add(i_Label);
         }
     }
 }
