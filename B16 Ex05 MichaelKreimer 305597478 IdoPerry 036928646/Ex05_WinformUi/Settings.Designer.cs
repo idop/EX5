cd92cb59289b4a700b4d242dcdb924432c98d11a
Michael Kreimer <michaelkreimer@gmail.com>
2016-05-31 23:26:34 +0300

    more changes in settings form

--- B16 Ex05 MichaelKreimer 305597478 IdoPerry 036928646/Ex05_WinformUi/Settings.Designer.cs
+++ B16 Ex05 MichaelKreimer 305597478 IdoPerry 036928646/Ex05_WinformUi/Settings.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -46,95 +47,59 @@
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Players:";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 27);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Player 1:";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(25, 50);
+            resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Player 2:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(79, 24);
+            resources.ApplyResources(this.textBox1, "textBox1");
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(76, 20);
-            this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(79, 47);
+            resources.ApplyResources(this.textBox2, "textBox2");
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(76, 20);
-            this.textBox2.TabIndex = 4;
-            this.textBox2.Text = "[Computer]";
             // 
             // checkBox1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(4, 50);
+            resources.ApplyResources(this.checkBox1, "checkBox1");
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(15, 14);
-            this.checkBox1.TabIndex = 5;
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 97);
+            resources.ApplyResources(this.label4, "label4");
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(61, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Board Size:";
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(194, 132);
+            resources.ApplyResources(this.label5, "label5");
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(30, 13);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "Cols:";
             // 
             // label6
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(22, 132);
+            resources.ApplyResources(this.label6, "label6");
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(34, 13);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "Rows";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(113, 185);
+            resources.ApplyResources(this.button1, "button1");
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 39);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Start!";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // nUDRows
             // 
-            this.nUDRows.Location = new System.Drawing.Point(65, 130);
+            resources.ApplyResources(this.nUDRows, "nUDRows");
             this.nUDRows.Maximum = new decimal(new int[] {
             10,
             0,
@@ -146,8 +111,6 @@
             0,
             0});
             this.nUDRows.Name = "nUDRows";
-            this.nUDRows.Size = new System.Drawing.Size(90, 20);
-            this.nUDRows.TabIndex = 10;
             this.nUDRows.Value = new decimal(new int[] {
             4,
             0,
@@ -156,7 +119,7 @@
             // 
             // nUDCols
             // 
-            this.nUDCols.Location = new System.Drawing.Point(230, 130);
+            resources.ApplyResources(this.nUDCols, "nUDCols");
             this.nUDCols.Maximum = new decimal(new int[] {
             10,
             0,
@@ -168,8 +131,6 @@
             0,
             0});
             this.nUDCols.Name = "nUDCols";
-            this.nUDCols.Size = new System.Drawing.Size(90, 20);
-            this.nUDCols.TabIndex = 11;
             this.nUDCols.Value = new decimal(new int[] {
             4,
             0,
@@ -178,7 +139,7 @@
             // 
             // Settings
             // 
-            this.ClientSize = new System.Drawing.Size(336, 240);
+            resources.ApplyResources(this, "$this");
             this.Controls.Add(this.nUDCols);
             this.Controls.Add(this.nUDRows);
             this.Controls.Add(this.button1);
@@ -192,7 +153,6 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Settings";
-            this.Text = "Settings";
             ((System.ComponentModel.ISupportInitialize)(this.nUDRows)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nUDCols)).EndInit();
             this.ResumeLayout(false);
