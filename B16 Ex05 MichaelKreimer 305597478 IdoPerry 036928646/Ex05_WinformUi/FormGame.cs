27785816d32a7263a13d733d4ab12dd3e7aa5650
idope <idope@DESKTOP-0K2SOPR>
2016-06-03 17:10:41 +0300

    game form

---
+++ B16 Ex05 MichaelKreimer 305597478 IdoPerry 036928646/Ex05_WinformUi/FormGame.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ex05_WinformUi
+{
+    public partial class FormGame : Form
+    {
+        private const int k_Margin = 15;
+        private const int k_ColumnSelectButtonWidth = 40;
+        private const int k_ColumnSelectButtonHeight = 30;
+        private const int k_BoardPieceButtonHeight = 70;
+        private const int k_LabelHeight = 20;
+        private Button[] buttonsColumnsSelect;
+        private int m_NumberOfRows;
+        private int m_NumberOfColumns;
+
+
+        public FormGame()
+        {
+            InitializeComponent();
+        }
+
+        public FormGame(int i_NumberOfRows, int i_NumberOfColumns)
+        {
+            m_NumberOfRows = i_NumberOfRows;
+            m_NumberOfColumns = i_NumberOfColumns;
+            buttonsColumnsSelect = new Button[i_NumberOfColumns];
+            initializeComponents();
+        }
+
+        private void initializeComponents()
+        {
+            this.Width = (m_NumberOfColumns + 2) * k_Margin + k_ColumnSelectButtonWidth * m_NumberOfColumns;
+            this.Height = (m_NumberOfRows + 3) * k_Margin + k_ColumnSelectButtonHeight + k_BoardPieceButtonHeight * m_NumberOfRows + k_LabelHeight;
+            for (int i = 0; i < m_NumberOfColumns; ++i)
+            {
+                buttonsColumnsSelect[i] = new Button();
+                buttonsColumnsSelect[i].Text = string.Format("{0}",i + 1);
+                buttonsColumnsSelect[i].Top = k_Margin;
+                buttonsColumnsSelect[i].Left = k_Margin * (i + 1) + k_ColumnSelectButtonWidth * i;
+                buttonsColumnsSelect[i].Width = k_ColumnSelectButtonWidth;
+                buttonsColumnsSelect[i].Height = k_ColumnSelectButtonHeight;
+                buttonsColumnsSelect[i].Click += buttonColumnSelect_Click;      
+                this.Controls.Add(buttonsColumnsSelect[i]);
+            }
+        }
+
+        private void buttonColumnSelect_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
